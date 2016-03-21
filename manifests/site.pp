a54b609ca8eaef01ba45d53b0afbd038046ecb20
sureshnataraj5 <suresh.nataraj@intel.com>
2016-03-21 14:54:12 +0530

    Exerices 7

--- manifests/site.pp
+++ manifests/site.pp
@@ -43,4 +43,12 @@ node default {
   # Example:
   #   class { 'my_class': }
   notify { "Hello, test my name is ${::hostname}": }
+  
+  file {'/etc/motd':
+    ensure => file,
+    owner => 'root',
+    group => 'root',
+    mode => '0644',
+    content => 'Basics of Puppet',
+  }
 }
